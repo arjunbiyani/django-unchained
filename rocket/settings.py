8930f9cd3d2fdb1d1f7d0a31bbabe4d6c8cdf694
Arjun Biyani <arjunbiyani@Arjuns-MacBook-Pro.local>
2019-09-06 16:42:05 +0530

    React Added

--- rocket/settings.py
+++ rocket/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Customers',
-    'rest_framework'
+    'rest_framework',
+    'react'
 ]
 
 MIDDLEWARE = [
--- rocket/urls.py
+++ rocket/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('react.urls')),
     path('', include('Customers.urls')),
     path('admin/', admin.site.urls),
 ]
