090db5e805d1d00fdc790010a7a920dcd3dbb888
Arjun <arjun@Arjuns-MacBook-Pro.local>
2020-04-19 01:24:04 +0530

    changes

---
+++ Users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=60)
+    created_at = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.id
+
+class settings(models.Model):
+    id = models.IntegerField
+    user_id= models.IntegerField
+    created_at = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.id
