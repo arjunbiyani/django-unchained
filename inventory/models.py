e14a25188a245dcda62be649d53fb9920ec96c73
Arjun Biyani <Arjunbiyani@ACKO522.local>
2020-05-28 12:45:45 +0530

    Refactored Model

--- inventory/models.py
+++ inventory/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class inventory(models.Model):
     id = models.IntegerField(primary_key=True)
     sku_code = models.CharField(max_length=60)
-    price = models.IntegerField
-    stock = models.IntegerField
+    price = models.IntegerField()
+    stock = models.IntegerField()
     created_by = models.CharField(max_length=60)
     created_at = models.DateTimeField('created at')
     updated_at = models.DateTimeField('updated at')
 
-    def __str__(self):
+    def __int__(self):
         return self.id
--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,18 @@
 from rest_framework import viewsets
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 from inventory.serializers import InventorySerializer
 from inventory.models import inventory
 
 class InventoryView(viewsets.ModelViewSet):
-    queryset = inventory.objects.all().order_by('id')
-    serializer_class = InventorySerializer
+    def get(self):
+        queryset = inventory.objects.all().order_by('id')
+        serializer_class = InventorySerializer(queryset, many=true)
+        return HttpResponse(serializer_class.data)
+    def post(self):
+        pass
+
+
 
 
