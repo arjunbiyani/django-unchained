8a2e8852be890c42506818dde5d2278082a1ce51
Arjun Biyani <Arjunbiyani@ACKO522.local>
2020-05-29 16:32:56 +0530

    Changed Migrations

--- inventory/models.py
+++ inventory/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 # Create your models here.
 class inventory(models.Model):
-    id = models.IntegerField(primary_key=True)
-    sku_code = models.CharField(max_length=60)
+    id = models.AutoField(primary_key=True)
+    sku_code = models.CharField(max_length=60, unique=True)
     price = models.IntegerField()
     stock = models.IntegerField()
     created_by = models.CharField(max_length=60)
-    created_at = models.DateTimeField('created at')
-    updated_at = models.DateTimeField('updated at')
+    created_at = models.DateTimeField('created at', auto_now_add=True)
+    updated_at = models.DateTimeField('updated at', auto_now_add=True)
 
     def __int__(self):
         return self.id
--- inventory/serializers.py
+++ inventory/serializers.py
@@ -5,4 +5,12 @@ class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = inventory
         fields = '__all__'
-
+    class Insert:
+        model = inventory
+        fields = ["sku_code",
+                  "price",
+                  "stock",
+                  "created_at",
+                  "created_by",
+                  "updated_at",
+                  ]
